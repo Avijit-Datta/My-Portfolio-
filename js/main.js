cf8e2be6e1fcb281304e6962197a347b5e5cf612
root <root@localhost>
2020-06-09 12:46:10 +0600

    publishing awesome pages 1591685170491

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// Created by Avijit Datta
+
+var x= prompt ("Enter your name😅");
+alert("Hi, "+ x + ". Hope you're well. It's my first portfolio! I've added many features in this! Please check them all. If you like my code don't forget to upvote & comment me how I can upgrade it. Thanks for visiting.💙");
+
+//DROP DOWN MENU JQUERY
+$(function() {
+  $(".nav").click(function() {
+    $(".section").slideToggle(400);
+  });
+});
